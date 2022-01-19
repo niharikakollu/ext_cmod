d26d712523473aa425a774f0c93930cf8ae7616d
niharikakollu <79960484+niharikakollu@users.noreply.github.com>
2022-01-19 13:19:04 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -23,10 +23,12 @@ static int helloworld_hello(lua_State* L) {
 // addition c module
 static int helloworld_add(lua_State* L)
 {
-    int num1 = luaL_checkinteger( L, 1 );
-  int num2 = luaL_checkinteger( L, 2 );
-    int tadd=intarith (L, LUA_OPADD, num1,num2);
-    lua_pushinteger( L, ( lua_Integer ) tadd );
+    int n= lua_gettop(L);
+  int num1 = lua_tonumber( L, 1 );
+  int num2 = lua_tonumber( L, 2 );
+    int tadd=num1+num2;
+     printf("number of arguments, %d:\n", n);
+   lua_pushnumber(L, tadd);
     return 1;
 }
 // helloworld_delete is called on garbage collection
