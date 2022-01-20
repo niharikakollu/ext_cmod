292dee26264b167b2aee2189f76f831e119547e0
niharikakollu <79960484+niharikakollu@users.noreply.github.com>
2022-01-20 19:26:16 +0530

    Update arith.c

--- arith.c
+++ arith.c
@@ -2,7 +2,7 @@
 #include "lauxlib.h"
 #include "lnodeaux.h"
 #include "module.h"
-
+#include "string.h"
 static const char* ARITHMETIC_METATABLE = NODEMCU_MODULE_METATABLE();
 // addition module
 static int arithmetic_add(lua_State* L)
@@ -40,14 +40,48 @@ static int arithmetic_mul(lua_State* L){
 // division module
  static int arithmetic_div(lua_State* L)
 {
+  char div_res[20];
   int n= lua_gettop(L);
-  int num1 = lua_tonumber( L, 1);
-  int num2 = lua_tonumber( L, 2);
+  float num1 = lua_tonumber( L, 1);
+  float num2 = lua_tonumber( L, 2);
   float div_test=num1/num2;
-  printf("number of arguments, %d:\nnum1 %d ,num2 %d\n", n,num1,num2);
+  sprintf(div_res, "%f",div_test);
+  printf("number of arguments, %d:\nnum1 %f ,num2 %f\n", n,num1,num2);
     lua_pushnumber( L, div_test );
     return 1;
 }
+// string concatination module
+static int string_concat(lua_State* L)
+{
+   char *sdata1,*sdata2;
+    size_t datalen;
+    datalen = lua_objlen( L, 1 );
+    sdata1=luaL_checklstring( L,1, &datalen );
+     datalen = lua_objlen( L, 2);
+    sdata2=luaL_checklstring( L,2, &datalen );
+    strcat(sdata1,sdata2);
+    lua_pushstring(L,sdata1);
+    return 1;
+}
+ static int load_arr(lua_State* L){
+  int data;
+  size_t datalen, i;
+   lua_newtable( L ); 
+     if( lua_istable( L,1)){
+     datalen = lua_objlen( L,1 );
+      for( i = 0; i < datalen; i ++ ){
+       lua_rawgeti( L, 1, i + 1 );
+       data= ( int )luaL_checkinteger( L, -1 );
+       lua_pop( L, 1 );
+       if( data< 0 || data> 255 )
+       return luaL_error( L, "table value does not fit in 1 byte" );
+       printf("table data %d\n", data);  
+	 lua_pushinteger(L, (lua_Integer )data);
+         lua_rawseti( L,-2, i + 1 );	
+      }
+     } 
+   return 1;
+ }
 LROT_BEGIN(arith_metatable)
 LROT_END(arith_metatable, NULL, 0)
 
@@ -56,6 +90,8 @@ LROT_FUNCENTRY(add, arithmetic_add)
 LROT_FUNCENTRY(sub, arithmetic_sub)
 LROT_FUNCENTRY(mul, arithmetic_mul)
 LROT_FUNCENTRY(div, arithmetic_div)
+LROT_FUNCENTRY(larr,load_arr)
+LROT_FUNCENTRY(sconcat,string_concat)
 LROT_END(module, NULL, 0)
 
 // module_init is invoked on device startup
